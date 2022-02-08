3e7084ddd537229e84dd32490984c59c55cecc1e
Zhiyu(Drew) Li <zyli2004@gmail.com>
2022-02-07 19:25:41 -0600

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,9 @@
- #!/bin/bash
+#!/bin/bash
  
- echo "compile"
+echo "compile"
+git clone https://github.com/NCAR/wrf_hydro_nwm_public.git /job/executable/WRFHYDRO
+cp  /job/data/setEnvar.sh /job/executable/WRFHYDRO/trunk/NDHMS/
+cd /job/executable/WRFHYDRO/trunk/NDHMS
+./configure 2
+./compile_offline_NoahMP.sh setEnvar.sh
+ls /job/executable/WRFHYDRO/trunk/NDHMS/Run -al
